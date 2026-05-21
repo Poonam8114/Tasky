827c6a2a35d99b17970a51c20c033152637d5d3b
poonamyadav <py48606@gmail.com>
2026-05-21 21:29:39 +0530

    create task api route

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import dns from "dns"
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
+import taskRoutes from "./routes/task.route.js"
 
 dns.setServers(['1.1.1.1',
   '8.8.8.8'
@@ -44,6 +45,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
+app.use("/api/tasks", taskRoutes)
 
 
 app.listen(3000, () => {
