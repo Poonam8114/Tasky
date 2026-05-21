325b74422cf7504569e6c532ff1fcccef82d05ca
poonamyadav <py48606@gmail.com>
2026-05-21 22:26:23 +0530

    update task api route

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -2,7 +2,8 @@ import express from "express"
 import { adminOnly, verifyToken } from "../utils/verifyUser.js"
 import {
   createTask, getTasks,
-    getTaskById
+    getTaskById,
+    updateTask
   
 } from "../controller/task.controller.js"
 
@@ -13,6 +14,7 @@ router.post("/create", verifyToken, adminOnly, createTask)
 router.get("/", verifyToken, getTasks)
 
 router.get("/:id", verifyToken, getTaskById)
+router.put("/:id", verifyToken, updateTask)
 
 
 export default router
