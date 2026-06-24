3139205dd4ea582229be8cc49597b3c66062c8bc
poonamyadav <py48606@gmail.com>
2026-06-24 15:04:52 +0530

    backend url setup

--- frontend/src/utils/axioInstance.js
+++ frontend/src/utils/axioInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+//const BASE_URL = "http://localhost:3000/api"
+
+const BASE_URL = "https://tasky-backend-e2cr.onrender.com"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
